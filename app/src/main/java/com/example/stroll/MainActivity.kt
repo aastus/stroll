57d6ed1eb9902e54836a42ed0ce29ab71ed0c612
LizaTiukh <lizatiuh2014@gmail.com>
2026-04-07 20:36:39 +0300

    feat: add navbar & 3 views

--- app/src/main/java/com/example/stroll/MainActivity.kt
+++ app/src/main/java/com/example/stroll/MainActivity.kt
@@ -2,24 +2,14 @@ package com.example.stroll
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.hardware.*
 import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.stroll.R
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-
-    private var sensorManager: SensorManager? = null
-    private var stepSensor: Sensor? = null
-
-    private var running = false
-    private var totalSteps = 0f
-    private var previousTotalSteps = 0f
-
-    private lateinit var stepsTakenTextView: TextView
+class MainActivity : AppCompatActivity() {
 
     private val PERMISSION_REQUEST_ACTIVITY_RECOGNITION = 100
 
@@ -27,75 +17,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        stepsTakenTextView = findViewById(R.id.tv_stepsTaken)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        // Permission
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navView.setupWithNavController(navController)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (checkSelfPermission(Manifest.permission.ACTIVITY_RECOGNITION)
-                != PackageManager.PERMISSION_GRANTED
-            ) {
+            if (checkSelfPermission(Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(
                     arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
                     PERMISSION_REQUEST_ACTIVITY_RECOGNITION
                 )
             }
         }
-
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if (stepSensor == null) {
-            Toast.makeText(this, "No Step Sensor!", Toast.LENGTH_LONG).show()
-        }
-
-        loadData()
-        resetSteps()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        running = true
-        stepSensor?.also {
-            sensorManager?.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        running = false
-        sensorManager?.unregisterListener(this)
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event == null) return
-
-        if (running) {
-            totalSteps = event.values[0]
-            val currentSteps = (totalSteps - previousTotalSteps).toInt()
-            stepsTakenTextView.text = currentSteps.toString()
-        }
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
-
-    private fun resetSteps() {
-        stepsTakenTextView.setOnLongClickListener {
-            previousTotalSteps = totalSteps
-            stepsTakenTextView.text = "0"
-            saveData()
-            true
-        }
-    }
-
-    private fun saveData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
-    }
-
-    private fun loadData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
-        previousTotalSteps = sharedPreferences.getFloat("key1", 0f)
     }
 }
--- app/src/main/java/com/example/stroll/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/stroll/ui/dashboard/DashboardFragment.kt
@@ -1,42 +1,104 @@
 package com.example.stroll.ui.dashboard
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.stroll.databinding.FragmentDashboardBinding
+import com.example.stroll.R
 
-class DashboardFragment : Fragment() {
+class DashboardFragment : Fragment(), SensorEventListener {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var sensorManager: SensorManager? = null
+    private var stepSensor: Sensor? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+    private lateinit var stepsTakenTextView: TextView
+    private lateinit var percentTextView: TextView
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        stepsTakenTextView = view.findViewById(R.id.tv_stepsTaken)
+        percentTextView = view.findViewById(R.id.tv_percent)
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            Toast.makeText(requireContext(), "No Step Sensor found!", Toast.LENGTH_LONG).show()
+        }
+
+        loadData()
+        resetSteps()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        stepSensor?.also {
+            sensorManager?.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) return
+
+        if (running) {
+            totalSteps = event.values[0]
+            val currentSteps = (totalSteps - previousTotalSteps).toInt()
+
+            stepsTakenTextView.text = currentSteps.toString()
+
+            val percent = (currentSteps * 100) / 10000
+            percentTextView.text = "$percent%"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    private fun resetSteps() {
+        stepsTakenTextView.setOnLongClickListener {
+            previousTotalSteps = totalSteps
+            stepsTakenTextView.text = "0"
+            percentTextView.text = "0%"
+            saveData()
+            true
+        }
+    }
+
+    private fun saveData() {
+        val sharedPreferences = requireActivity().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = requireActivity().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        previousTotalSteps = sharedPreferences.getFloat("key1", 0f)
     }
 }
